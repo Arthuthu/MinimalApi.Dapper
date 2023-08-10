a5cdaf298f008e4d4863ca9f921fb8a38d9e9ce4
Arthuthu <arthurgeromello@hotmail.com>
2023-08-09 21:28:23 -0300

    Add project files.

---
+++ MilanDatabase/dbo/Customers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [DateOfBirth] DATETIME2 NULL,
+
+)
